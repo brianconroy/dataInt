2b149c2c2bf35cad756acbe985e1fbc8f98e12c3
Brian Conroy <bconroy@g.hmc.edu>
2019-09-01 00:51:00 -0400

    add covariates to locational model

---
+++ R/hmc_mod2.R
@@ -0,0 +1,214 @@
+
+
+betaLocHmcUpdate <- function(y.l, w, x.l, beta.loc, delta, L){
+  
+  # sample random momentum
+  p0 <- matrix(rnorm(length(beta.loc)))
+  
+  # simulate Hamiltonian dynamics
+  bcurr <- matrix(beta.loc)
+  pStar <- p0 - 0.5 * delta * dU_beta_loc(y.l, w, x.l, beta.loc)
+  
+  # first full step for position
+  bStar <- bcurr + delta*pStar
+  
+  # full steps
+  for (jL in 1:c(L-1)){
+    # momentum
+    pStar <- pStar - delta * dU_beta_loc(y.l, w, x.l, bStar)
+    
+    # position
+    bStar <- bStar + delta*pStar
+  }
+  
+  # last half step
+  pStar <- pStar - 0.5 * delta * dU_beta_loc(y.l, w, x.l, bStar)
+  
+  # evaluate energies
+  U0 <- U_beta_loc(y.l, w, x.l, bcurr)
+  UStar <- U_beta_loc(y.l, w, x.l, bStar)
+  
+  K0 <- K(p0)
+  KStar <- K(pStar)
+  
+  # accept/reject
+  alpha <- min(1, exp((U0 + K0) - (UStar + KStar)))
+  if (is.na(alpha)){
+    alpha <- 0
+  }
+  
+  if (runif(1, 0, 1) < alpha){
+    bnext <- bStar
+    accept <- 1
+  } else {
+    bnext <- bcurr
+    accept <- 0
+  }
+  
+  out <- list()
+  out$b <- bnext
+  out$accept <- accept
+  out$a <- alpha
+  return(out)
+  
+}
+
+
+dU_beta_loc <- function(y.l, w, x.l, beta.loc){
+  
+  grad <- array(0, c(length(beta.loc), 1))
+  
+  # location contribution
+  loc_pred <- x.l %*% beta.loc + w
+  for (j in 1:length(beta.loc)){
+    for (i in 1:length(w)){
+      grad[j] <- grad[j] + x[i,j] * (y.l[i] - expit(loc_pred[i]))
+    }
+  }
+  
+  # prior contribution
+  prior_var_inv <- diag(rep(1/100, length(beta.loc)))
+  grad <- grad + t(-t(beta.loc) %*% prior_var_inv)
+  return(-grad)
+  
+}
+
+
+U_beta_loc <- function(y.l, w, x.l, beta.loc){
+  
+  # likelihood
+  logd <- 0
+  lin_preds <- x.l %*% beta.loc + w
+  for (i in 1:length(y.l)){
+    logd <- logd + dbinom(y.l[i], size=1, prob=expit(lin_preds[i]), log=T)
+  }
+  
+  # prior
+  prior_beta_mean <- rep(0, length(beta.loc))
+  prior_beta_var <- diag(rep(100, length(beta.loc)))
+  
+  logd <- logd + dmvnorm(as.numeric(beta.loc), prior_beta_mean, prior_beta_var, log=T)
+  
+  return(-logd)
+  
+}
+
+
+
+Uw_v2 <- function(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, w, sigma, loc.stats, offset){
+  
+  logd <- 0
+  
+  # likelihood: locations
+  loc_pred <- x.l %*% beta.loc + w
+  for (i in 1:length(y.l)){
+    logd <- logd + dbinom(y.l[i], size=1, prob=expit(loc_pred[i]), log=T)
+  }
+  
+  # likelihood: case counts
+  w.sub <- w[as.logical(loc.stats$status)]
+  count_pred <- offset + x.c %*% beta.ca + alpha.ca * w.sub
+  rates <- exp(count_pred)
+  for (i in 1:length(y.ca)){
+    logd <- logd + dpois(y.ca[i], lambda=rates[i], log=T)
+  }
+  
+  # likelihood: control counts
+  count_pred <- offset + x.c %*% beta.co + alpha.co * w.sub
+  rates <- exp(count_pred)
+  for (i in 1:length(y.co)){
+    logd <- logd + dpois(y.co[i], lambda=rates[i], log=T)
+  }
+  
+  # prior
+  logd <- logd + dmvnorm(as.numeric(w), rep(0, length(w)), sigma, log=T)
+  
+  return(-logd)
+  
+}
+
+
+dU_w_v2 <- function(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, w, sigma.inv, loc.stats, offset){
+  
+  grad <- array(0, c(length(w), 1))
+  
+  # location contribution
+  loc.pred <- x.l %*% beta.loc + w
+  for (i in 1:length(w)){
+    grad[i] <- grad[i] + y.l[i] - expit(loc.pred[i])
+  }
+  
+  # case contribution
+  lin.count <- offset + x.c %*% beta.ca
+  for (i in 1:length(loc.stats$ids)){
+    id.i <- loc.stats$ids[i]
+    grad[id.i] <- grad[id.i] + alpha.ca * (y.ca[i] - exp(lin.count[i] + alpha.ca * w[id.i]))
+  }
+  
+  # control contribution
+  lin.count <- offset + x.c %*% beta.co
+  for (i in 1:length(loc.stats$ids)){
+    id.i <- loc.stats$ids[i]
+    grad[id.i] <- grad[id.i] + alpha.co * (y.co[i] - exp(lin.count[i] + alpha.co * w[id.i]))
+  }
+  
+  # prior contribution
+  grad <- grad + t(-t(w) %*% sigma.inv)
+  
+  return(-grad)
+  
+}
+
+
+wHmcUpdateV2 <- function(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, w, sigma, sigma.inv, loc.stats, delta, L, offset){
+  
+  # sample random momentum
+  p0 <- matrix(rnorm(length(w)))
+  
+  # simulate Hamiltonian dynamics
+  wcurr <- matrix(w)
+  pStar <- p0 - 0.5 * delta * dU_w_v2(y.l, x.c, x.l,  y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, wcurr, sigma.inv, loc.stats, offset)
+  
+  # first full step for position
+  wStar <- wcurr + delta*pStar
+  
+  # full steps
+  for (jL in 1:c(L-1)){
+    # momentum
+    pStar <- pStar - delta * dU_w_v2(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, wStar, sigma.inv, loc.stats, offset)
+    
+    # position
+    wStar <- wStar + delta*pStar
+  }
+  
+  # last half step
+  pStar <- pStar - 0.5 * delta * dU_w_v2(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, wStar, sigma.inv, loc.stats, offset)
+  
+  # evaluate energies
+  U0 <- Uw_v2(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, wcurr, sigma, loc.stats, offset)
+  UStar <- Uw_v2(y.l, x.c, x.l, y.ca, alpha.ca, beta.ca, beta.loc, y.co, alpha.co, beta.co, wStar, sigma, loc.stats, offset)
+  
+  K0 <- K(p0)
+  KStar <- K(pStar)
+  
+  # accept/reject
+  alpha <- min(1, exp((U0 + K0) - (UStar + KStar)))
+  if (is.na(alpha)){
+    alpha <- 0
+  }
+  
+  if (runif(1, 0, 1) < alpha){
+    wnext <- wStar
+    accept <- 1
+  } else {
+    wnext <- wcurr
+    accept <- 0
+  }
+  
+  out <- list()
+  out$w <- wnext
+  out$accept <- accept
+  out$a <- alpha
+  return(out)
+  
+}
---
+++ studies/devPrefSamplingGpMod2.R
@@ -0,0 +1,129 @@
+library(plyr)
+library(R.utils)
+library(ggplot2)
+library(gridExtra)
+library(mvtnorm)
+sourceDirectory('Documents/research/dataInt/R/')
+
+
+#### Prism Principal Components
+caPr <- load_prism_pcs2()
+caPr.disc <- aggregate(caPr, fact=9)
+n_values(caPr.disc[[1]])
+plot(caPr.disc)
+
+
+#### Simulate gaussian process
+Phi <- 12
+Theta <- 6
+cells.all <- c(1:ncell(caPr.disc))[!is.na(values(caPr.disc[[1]]))]
+coords <- xyFromCell(caPr.disc, cell=cells.all)
+d <- as.matrix(dist(coords, diag=TRUE, upper=TRUE))
+Sigma <- Exponential(d, range=Theta, phi=Phi)
+set.seed(40)
+W <- mvrnorm(n=1, mu=rep(0, length(cells.all)), Sigma)
+N <- length(W)
+hist(W)
+
+
+#### Simulate locations
+beta.samp <- c(-2, 1, -0.5)
+locs <- simBernoulliLocCov(caPr.disc, beta.samp, w=W, seed=56)
+sum(locs$status)
+hist(locs$w)
+plot(caPr.disc[[1]])
+points(locs$coords)
+
+
+#### Simulate counts given locations
+Alpha.case <- 1.75
+Alpha.ctrl <- 0.25
+beta.case <- c(0.25, 1, -0.5)
+beta.ctrl <- c(4, 1, -0.5)
+cov.disc <- caPr.disc
+case.data <- simConditionalGp2(cov.disc, locs, beta.case, Alpha.case, W, seed=42)
+ctrl.data <- simConditionalGp2(cov.disc, locs, beta.ctrl, Alpha.ctrl, W, seed=40)
+print(sum(case.data$y)/sum(case.data$y + ctrl.data$y))
+print(sum(case.data$y))
+print(sum(ctrl.data$y))
+
+
+#### Fit
+data <- list(
+  loc=locs,
+  case.data=case.data,
+  ctrl.data=ctrl.data
+)
+
+
+m_aca <- 1000
+m_aco <- 1000
+m_ca <- 1000
+m_co <- 1000
+m_loc <- 1000
+m_w <- 1000
+
+L_w <- 8
+L_ca <- 8
+L_co <- 8
+L_a_ca <- 8
+L_a_co <- 8
+L_loc <- 8
+proposal.sd.theta <- 0.15
+
+target_aca=0.75
+target_aco=0.75
+target_ca=0.75
+target_co=0.75
+target_w=0.75
+target_loc=0.75
+
+self_tune_w=TRUE
+self_tune_aca=TRUE
+self_tune_aco=TRUE
+self_tune_ca=TRUE
+self_tune_co=TRUE
+self_tune_loc=TRUE
+
+beta_locs_initial=rep(0,0,0)
+beta_ca_initial=beta.case
+beta_co_initial=beta.ctrl
+alpha_ca_initial=Alpha.case
+alpha_co_initial=Alpha.ctrl
+theta_initial=Theta
+phi_initial=Phi
+w_initial=W
+
+prior_alpha_ca_mean <- Alpha.case
+prior_alpha_ca_var <- 4
+prior_alpha_co_mean <- Alpha.ctrl
+prior_alpha_co_var <- 4
+prior_theta <- c(6, 1)
+prior_phi <- c(18, 204)
+
+n.sample <- 200
+burnin <- 0
+
+# ToDo: estimate initial values
+
+output <- prefSampleGpV2(data, d, n.sample, burnin, 
+                           L_w, L_ca, L_co, L_a_ca, L_a_co,
+                           proposal.sd.theta=proposal.sd.theta,
+                           m_aca=m_aca, m_aco=m_aco, m_ca=m_ca, m_co=m_co, m_w=m_w, 
+                           target_aca=target_aca, target_aco=target_aco, target_ca=target_ca, target_co=target_co, target_w=target_w, target_loc=target_loc,
+                           self_tune_w=TRUE, self_tune_aca=TRUE, self_tune_aco=TRUE, self_tune_ca=TRUE, self_tune_co=TRUE, self_tune_loc=TRUE,
+                           beta_ca_initial=beta_ca_initial, beta_co_initial=beta_co_initial, alpha_ca_initial=alpha_ca_initial, alpha_co_initial=alpha_co_initial, beta_loc_initial=beta_loc_initial,
+                           theta_initial=theta_initial, phi_initial=phi_initial, w_initial=w_initial,
+                           prior_phi=prior_phi, prior_theta=prior_theta, prior_alpha_ca_var=prior_alpha_ca_var, prior_alpha_co_var=prior_alpha_co_var)
+
+par(mfrow=c(1,3))
+plot(output$samples.beta.loc[,1], type='l'); abline(h=beta.samp[1], col=2)
+plot(output$samples.beta.loc[,2], type='l'); abline(h=beta.samp[2], col=2)
+plot(output$samples.beta.loc[,3], type='l'); abline(h=beta.samp[3], col=2)
+
+par(mfrow=c(1,1))
+plot(colMeans(output$samples.w), W); abline(0, 1, col=2)
+
+par(mfrow=c(1,2))
+plot(output$samples.alpha.ca, type='l'); abline(h=Alpha.case, col=2)
+plot(output$samples.alpha.co, type='l'); abline(h=Alpha.ctrl, col=2)
